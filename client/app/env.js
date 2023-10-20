656320ae48d2bb4ac71fc3fcbc51c8f1b2f6e1f3
Jarek Garcia <prodijee@hotmail.com>
2023-10-20 09:45:55 -0600

    PlantSpace launch

---
+++ client/app/env.js
@@ -0,0 +1,18 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'jdcode1auth.us.auth0.com'
+export const audience = 'https://JDcode1.com'
+export const clientId = '107NIYLd3nMVCVjwHecvRf8fI26E75MM'
+
+// # username:  prodijee
+
+// # password:  bov5AwInYajzoBso
+
+// # mongodb:  mongodb+srv://prodijee:<password>@jdcode1.j5otel4.mongodb.net/?retryWrites=true&w=majority
+
+// # domain:  jdcode1auth.us.auth0.com
+
+// # client id:  107NIYLd3nMVCVjwHecvRf8fI26E75MM
+
+// # audience/identifier:  https://JDcode1.com
