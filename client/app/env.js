ffae50de343e53ce04dd0662b85ace45e3029fe5
Blake Lawatch <blakelawatch@gmail.com>
2023-10-23 08:32:58 -0600

    Real fake posts made

--- client/app/env.js
+++ client/app/env.js
@@ -5,14 +5,4 @@ export const domain = 'jdcode1auth.us.auth0.com'
 export const audience = 'https://JDcode1.com'
 export const clientId = '107NIYLd3nMVCVjwHecvRf8fI26E75MM'
 
-// # username:  prodijee
 
-// # password:  bov5AwInYajzoBso
-
-// # mongodb:  mongodb+srv://prodijee:<password>@jdcode1.j5otel4.mongodb.net/?retryWrites=true&w=majority
-
-// # domain:  jdcode1auth.us.auth0.com
-
-// # client id:  107NIYLd3nMVCVjwHecvRf8fI26E75MM
-
-// # audience/identifier:  https://JDcode1.com
